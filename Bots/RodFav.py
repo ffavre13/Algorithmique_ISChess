573f381af0d21a9d6fad3fd87ce11f2695962483
ffavre13 <192215936+ffavre13@users.noreply.github.com>
2025-12-04 15:38:35 +0100

    Positions of pawn, king and bishop

--- Bots/RodFav.py
+++ Bots/RodFav.py
@@ -5,18 +5,180 @@ import random
 
 def get_move_king(y,x,board):
     possible_positions = []
+
+    # en haut a gauche
+    posy = y + 1
+    posx = x + 1
+    if posx < len(board[0]) and posy < len(board) and posx > 0 and posy > 0:
+        if board[posy][posx] == '':
+            possible_positions.append((posy,posx))
+        elif board[posy][posx][1] != board[y][x][1]:
+            possible_positions.append((posy,posx))
+
+    # au mid a gauche
+    posy = y
+    posx = x + 1
+    if posx < len(board[0]) and posy < len(board) and posx > 0 and posy > 0:
+        if board[posy][posx] == '':
+            possible_positions.append((posy,posx))
+        elif board[posy][posx][1] != board[y][x][1]:
+            possible_positions.append((posy,posx))
+
+    # en bas a gauche
+    posy = y - 1
+    posx = x + 1
+    if posx < len(board[0]) and posy < len(board) and posx > 0 and posy > 0:
+        if board[posy][posx] == '':
+            possible_positions.append((posy,posx))
+        elif board[posy][posx][1] != board[y][x][1]:
+            possible_positions.append((posy,posx))
+    
+    # en haut
+    posy = y + 1
+    posx = x
+    if posx < len(board[0]) and posy < len(board) and posx > 0 and posy > 0:
+        if board[posy][posx] == '':
+            possible_positions.append((posy,posx))
+        elif board[posy][posx][1] != board[y][x][1]:
+            possible_positions.append((posy,posx))
+
+    # en bas
+    posy = y - 1
+    posx = x
+    if posx < len(board[0]) and posy < len(board) and posx > 0 and posy > 0:
+        if board[posy][posx] == '':
+            possible_positions.append((posy,posx))
+        elif board[posy][posx][1] != board[y][x][1]:
+            possible_positions.append((posy,posx))
+
+    # en haut a droite
+    posy = y + 1
+    posx = x - 1
+    if posx < len(board[0]) and posy < len(board) and posx > 0 and posy > 0:
+        if board[posy][posx] == '':
+            possible_positions.append((posy,posx))
+        elif board[posy][posx][1] != board[y][x][1]:
+            possible_positions.append((posy,posx))
+
+    # au mid a droite
+    posy = y
+    posx = x - 1
+    if posx < len(board[0]) and posy < len(board) and posx > 0 and posy > 0:
+        if board[posy][posx] == '':
+            possible_positions.append((posy,posx))
+        elif board[posy][posx][1] != board[y][x][1]:
+            possible_positions.append((posy,posx))
+
+    # en bas a droite
+    posy = y - 1
+    posx = x - 1
+    if posx < len(board[0]) and posy < len(board) and posx > 0 and posy > 0:
+        if board[posy][posx] == '':
+            possible_positions.append((posy,posx))
+        elif board[posy][posx][1] != board[y][x][1]:
+            possible_positions.append((posy,posx))
+
+    print(possible_positions)
     return possible_positions
 
 def get_move_queen(y,x,board):
     possible_positions = []
+
+    ## Cardinales 
+
+    ## Diagonale
+    #Direction en haut a gauche
+    for i in range(1,min(len(board)-y,len(board[0])-x)):
+        if board[y+i][x+i] == '':
+            possible_positions.append((y+i,x+i))
+        elif board[y+i][x+i][1] != board[y][x][1]:
+            possible_positions.append((y+i,x+i))
+            break
+        else:
+            break
+
+
+    #Direction en haut a droite
+    for i in range(1,min(len(board)-y,x+1)):
+        if board[y+i][x-i] == '':
+            possible_positions.append((y+i,x-i))
+        elif board[y+i][x-i][1] != board[y][x][1]:
+            possible_positions.append((y+i,x-i))
+            break
+        else:
+            break
+
+    #Direction en bas a gauche
+    for i in range(1,min(y+1,len(board[0])-x)):
+        if board[y-i][x+i] == '':
+            possible_positions.append((y-i,x+i))
+        elif board[y-i][x+i][1] != board[y][x][1]:
+            possible_positions.append((y-i,x+i))
+            break
+        else:
+            break
+
+    #Direction en bas a droite
+    for i in range(1,min(y+1,x+1)):
+        if board[y-i][x-i] == '':
+            possible_positions.append((y-i,x-i))
+        elif board[y-i][x-i][1] != board[y][x][1]:
+            possible_positions.append((y-i,x-i))
+            break
+        else:
+            break
+
     return possible_positions
 
 def get_move_knight(y,x,board):
     possible_positions = []
+
     return possible_positions
 
 def get_move_bishop(y,x,board):
     possible_positions = []
+
+    #Direction en haut a gauche
+    for i in range(1,min(len(board)-y,len(board[0])-x)):
+        if board[y+i][x+i] == '':
+            possible_positions.append((y+i,x+i))
+        elif board[y+i][x+i][1] != board[y][x][1]:
+            possible_positions.append((y+i,x+i))
+            break
+        else:
+            break
+
+
+    #Direction en haut a droite
+    for i in range(1,min(len(board)-y,x+1)):
+        if board[y+i][x-i] == '':
+            possible_positions.append((y+i,x-i))
+        elif board[y+i][x-i][1] != board[y][x][1]:
+            possible_positions.append((y+i,x-i))
+            break
+        else:
+            break
+
+    #Direction en bas a gauche
+    for i in range(1,min(y+1,len(board[0])-x)):
+        if board[y-i][x+i] == '':
+            possible_positions.append((y-i,x+i))
+        elif board[y-i][x+i][1] != board[y][x][1]:
+            possible_positions.append((y-i,x+i))
+            break
+        else:
+            break
+
+    #Direction en bas a droite
+    for i in range(1,min(y+1,x+1)):
+        if board[y-i][x-i] == '':
+            possible_positions.append((y-i,x-i))
+        elif board[y-i][x-i][1] != board[y][x][1]:
+            possible_positions.append((y-i,x-i))
+            break
+        else:
+            break
+        
     return possible_positions
 
 def get_move_rook(y,x,board):
@@ -53,8 +215,8 @@ def get_all_possible_moves(player_sequence, board):
     for i in range(len(board)):
         for j in range(len(board[0])):
             if board[i][j] != '':
-                if board[i][j].string()[1] == current_player:
-                    match board[i][j].string()[0]:
+                if board[i][j][1] == current_player:
+                    match board[i][j][0]:
                         case "k":
                             for position in get_move_king(i,j,board):
                                 moves.append(((i,j),position))
@@ -80,9 +242,10 @@ def chess_bot(player_sequence, board, time_budget, **kwargs):
     start_time = time.time()
     moves = get_all_possible_moves(player_sequence, board)
     current_best_move = moves[random.randint(0,len(moves)-1)]
-
+    return current_best_move
+    
     while True:
-        if (time.time() - start_time >= time_budget - 0.05):
+        if (time.time() - start_time >= time_budget - 0.1):
             return current_best_move
         
 register_chess_bot("RodFav", chess_bot)
