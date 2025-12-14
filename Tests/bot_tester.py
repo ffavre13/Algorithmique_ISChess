e8f463e49cfcac16761bc1d49eabda2367d62cb9
ffavre13 <192215936+ffavre13@users.noreply.github.com>
2025-12-14 13:53:39 +0100

    Improve bot tester and fix Random Eater

--- Tests/bot_tester.py
+++ Tests/bot_tester.py
@@ -1,6 +1,9 @@
 import sys, os
 
 import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import time
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from Bots import *
@@ -36,13 +39,15 @@ class BoardManager:
 
 def play_game(player1, player2, board_manager, max_rounds=10):
     board = np.copy(board_manager.board)
+    result = [0,0,0,0] # player1_win, player2_win, draws, nb_round
     for round_num in range(max_rounds):
             # BoardManager.print_board(board)
-            
+            result[3] += 1
             move1 = player1("0w0", board, 1)
 
             if board[move1[1][0]][move1[1][1]] == 'kb':
-                return 'player1_wins'
+                result[0] += 1
+                return result
             
             if move1[1][0] == 7 and board[move1[0][0]][move1[0][1]] == 'pw':
                 board[move1[1][0]][move1[1][1]] = 'qw'
@@ -56,7 +61,8 @@ def play_game(player1, player2, board_manager, max_rounds=10):
 
             
             if board[move2[1][0]][move2[1][1]]  == 'kw':
-                return 'player2_wins'
+                result[1] += 1
+                return result
             
             if move2[1][0] == 7 and board[move2[0][0]][move2[0][1]] == 'pb':
                 board[move2[1][0]][move2[1][1]] = 'qb'
@@ -66,19 +72,28 @@ def play_game(player1, player2, board_manager, max_rounds=10):
                 board[move2[0][0]][move2[0][1]] = ''
             board = np.rot90(board, 2)
         
-    return 'draws'
+    result[2] += 1
+    return result
 
 def main():
     all_bots = []
-    print("===== Please choose the fist player (Don't select ManualMover) =====")
+    all_bots_name = []
+    all_results = []
+    print("===== Please choose the first player (Don't select ManualMover) =====")
     for i,bot in enumerate(ChessBotList.CHESS_BOT_LIST):
         print(str(i) + ") " + str(bot))
+        all_bots_name.append(str(bot))
         all_bots.append(ChessBotList.CHESS_BOT_LIST[bot])
 
-    player1 = all_bots[int(input("First player: "))]
-    player2 = all_bots[int(input("Second player: "))]
+
+    player1_id = int(input("First player: "))
+    player2_id = int(input("Second player: "))
+    player1 = all_bots[player1_id]
+    player2 = all_bots[player2_id]
     max_nb_round = int(input("Enter the maximum number of round per game: "))
     nb_games =  int(input("Enter the number of game to play: "))
+
+    number = int(time.time())
 
     results = {
         'player1_wins': 0,
@@ -88,22 +103,42 @@ def main():
 
     board_manager = BoardManager()
 
+    with open(f"Tests/data/{all_bots_name[player1_id]}_vs_{str(all_bots_name[player2_id])}_{number}.csv", 'w', newline='') as csvfile:
+        filewriter = csv.writer(csvfile, delimiter=';')
+        filewriter.writerow(['game_number','player1_win','player2_win','draw','nb_round'])
 
     for game_num in range(nb_games):
-        # print(f"{'='*60}\n")
-        # print(f"Game {game_num + 1}/{nb_games}")
-        # print(f"{'='*60}\n")
         current_result = play_game(player1, player2, board_manager, max_nb_round)
-        results[current_result] += 1
+        results['player1_wins'] += current_result[0]
+        results['player2_wins'] += current_result[1]
+        results['draws'] += current_result[2]
 
-    print(f"\n{'='*60}")
-    print("FINAL RESULTS")
+        with open(f"Tests/data/{all_bots_name[player1_id]}_vs_{str(all_bots_name[player2_id])}_{number}.csv", 'a', newline='') as csvfile:
+            filewriter = csv.writer(csvfile, delimiter=';')
+            filewriter.writerow([game_num+1]+current_result)
+
+        all_results.append([game_num+1]+current_result)
+
     print(f"{'='*60}")
     print(f"player1 (White): {results['player1_wins']} wins")
     print(f"player2 (Black): {results['player2_wins']} wins")
     print(f"Draws: {results['draws']}")
     print(f"Total games: {nb_games}")
-    print(f"{'='*60}\n")
+    print(f"{'='*60}")
+
+    plt.bar([f"{all_bots_name[player1_id]}",f"{str(all_bots_name[player2_id])}",'draws'],[results['player1_wins'],results['player2_wins'],results['draws']])
+    plt.ylabel("Number of wins")
+    plt.title(f"{all_bots_name[player1_id]} vs {str(all_bots_name[player2_id])} - number of wins")
+    plt.savefig(f"Tests/data/barplot_{all_bots_name[player1_id]}_vs_{str(all_bots_name[player2_id])}_{number}.svg", format="svg")
+    plt.close()
+
+    values = [e[4] for e in all_results]
+    plt.hist(values,bins=range(min(values), max(values) + 2))
+    plt.xlabel("Number of moves to finish the game")
+    plt.ylabel("Number of games")
+    plt.title(f"{all_bots_name[player1_id]} vs {str(all_bots_name[player2_id])} - move per game")
+    plt.savefig(f"Tests/data/histogram_{all_bots_name[player1_id]}_vs_{str(all_bots_name[player2_id])}_{number}.svg", format="svg")
+    plt.close()
 
 if __name__ == "__main__":
     main()
